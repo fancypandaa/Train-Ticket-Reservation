5a44d1ed6f6f75a7994138ba0bbaea5078e1056f
liosub <mlio64565@gmail.com>
2023-08-01 23:22:10 +0100

    add schedule services and test methods

--- src/main/java/spring/sys/train/services/ScheduleService.java
+++ src/main/java/spring/sys/train/services/ScheduleService.java
@@ -5,7 +5,8 @@ import spring.sys.train.models.Schedule;
 import java.util.*;
 public interface ScheduleService {
     Set<Schedule> getSchedules();
-    Schedule findScheduleById(Long Id);
-    ScheduleCommand findScheduleByTrainId(Long trainId);
+    Schedule findScheduleById(Long Id) throws Exception;
+    ScheduleCommand findScheduleByCommandId(Long Id);
+    Set<ScheduleCommand> findSchedulesByTrainId(Long trainId);
 
 }
