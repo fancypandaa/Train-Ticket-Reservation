2c0a055840cdbd08a365c41aaf7cbbc1de9c3b0a
liosub <mlio64565@gmail.com>
2023-08-14 16:39:12 +0100

    add swagger

--- src/main/java/spring/sys/train/config/SecurityConfig.java
+++ src/main/java/spring/sys/train/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+        .antMatchers("/**", "/swagger-resources").permitAll()
                 .antMatchers("/**")
                 .permitAll()
                 .and().csrf().disable();
